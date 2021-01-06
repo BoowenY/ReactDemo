348dfe5d707d9ead5ade9e755f43c332b6351298
“anger” <2455927306@qq.com>
2021-01-06 10:13:17 +0800

    this.state.list.spplice()

--- src/Xjj.js
+++ src/Xjj.js
@@ -4,7 +4,7 @@ class Xjj extends Component {
     super(props);
     this.state = {
       inputValue: "???",
-      list: [],
+      list: ['基础', 'addition'],
     };
   }
   render() {
@@ -12,10 +12,14 @@ class Xjj extends Component {
       <Fragment>
         <div>
           <input value= {this.state.inputValue} onChange = {this.inputChange.bind(this)}/>
-          <button>add service</button>
+          <button onClick = {this.addList.bind(this)}>add service</button>
           <ul>
-            <li>Anmo</li>
-            <li>Tuibei</li>
+            {
+              this.state.list.map((item, index) => {
+                return <li key={index + item}
+                onClick = {this.deleteItem.bind(this, index)}>{item}</li>
+              })
+            }
           </ul>
         </div>
       </Fragment>
@@ -26,6 +30,20 @@ class Xjj extends Component {
     console.log(this);
     this.setState({
         inputValue: e.target.value
+    })
+  }
+  addList(){
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: '',
+    })
+  }
+  deleteItem(index){
+    console.log(index);
+    let list = this.state.list;
+    list.splice(index, 1);
+    this.setState({
+      list: list,
     })
   }
 }
