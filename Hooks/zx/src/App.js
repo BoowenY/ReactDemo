9ebae57f1554135be5efd64782aaeb89fc84d6ea
an-ger <aiboowen@gmail.com>
2021-02-08 21:27:09 +0800

    Hooks

--- Hooks/zx/src/App.js
+++ Hooks/zx/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import LikeButton from './components/LikeButton';
 import MouseTracker from './components/MouseTracker';
 import DogShow from './components/DogShow';
-
+import useMousePosition from './hooks/useMousePosition'
 function App() {
+  const position = useMousePosition()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{position.x}</h1>
         <LikeButton/>
         <MouseTracker/>
         <DogShow/>
