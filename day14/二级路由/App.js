6974cf4a101122c786c10cca3629f42ac668033f
an-ger <aiboowen@gmail.com>
2021-02-12 18:13:20 +0800

    二级路由

---
+++ day14/二级路由/App.js
@@ -0,0 +1,19 @@
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import react from 'react'
+import {Route, Redirect, Switch, NavLink } from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+      <Route path="/about" component={About}/>
+      <Route path="/home" component={Home}/>
+      <Redirect to="/about/"/>
+      </Switch>
+      <NavLink to="/home">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+    </div>
+  );
+}
+
+export default App;
