14475235cd1b39a0a1fd292379c78ab9ef218b22
an-ger <aiboowen@gmail.com>
2021-02-17 23:38:32 +0800

    Redux完结撒花

---
+++ day15/src/App.js
@@ -0,0 +1,13 @@
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
+function App() {
+  return (
+    <div className="App">
+      <button>click</button>
+      <Button type="danger">antd</Button>
+      <Button >antd</Button>
+    </div>
+  );
+}
+
+export default App;
