240199f4fbe654447562da7412e9ea695901918e
an-ger <aiboowen@gmail.com>
2021-03-02 22:52:20 +0800

    高阶组件

---
+++ day_22/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react'
+class App extends PureComponent {
+    render() {
+        return (
+            <div>App</div>
+        )
+    }
+}
+function  enhance (Comp) {
+    class newComponent extends PureComponent {
+        render() {
+            return <Comp/>
+        }
+    }
+    newComponent.displayName = "why"
+    return newComponent;
+}
+export default enhance(App)
