988474cd7d5716ed3f7fea3b9c7e022bb8c08e5f
an-ger <aiboowen@gmail.com>
2021-03-14 23:23:36 +0800

    使用外部文件引入

---
+++ day_25/引入css样式/App.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import Home from './cmp/Home';
+class App extends PureComponent {
+    render() {
+        return(
+            <div>App
+                <Home/>
+            </div>
+        )
+    }
+}
+export default App;
---
+++ day_25/引入css样式/cmp/Home/index.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from "react";
+import "./style.css";
+export default class Home extends PureComponent {
+  render() {
+    return (
+      <div>
+        <div className="title">title</div>
+      </div>
+    );
+  }
+} 
