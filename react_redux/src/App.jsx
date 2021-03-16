2e0def4e0f342ea968ce6df8737401e83f4e4ba9
an-ger <aiboowen@gmail.com>
2021-03-16 22:22:15 +0800

    库

--- react_redux/src/App.jsx
+++ react_redux/src/App.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-import About from './About'
-import Home from './Home'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Home/>
-                <hr/>
-                <About/>
+                App
             </div>
         )
     }
